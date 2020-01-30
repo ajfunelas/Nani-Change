e78585fe9e86a25c66835528c76966c2175d70d7
ajfunelas <31107247+ajfunelas@users.noreply.github.com>
2020-01-31 00:21:35 +0800

    Create main.go
    Initial

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func dropsau() {
+	dropsauURL := "https://dropsau.com/pages/memberships"
+	doc, err := goquery.NewDocument(dropsauURL)
+}
