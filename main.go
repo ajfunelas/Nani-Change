3de933fc3d634f69d5de893d6421b2844fec672d
ajfunelas <31107247+ajfunelas@users.noreply.github.com>
2020-01-31 00:32:45 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"strings"
+	// "golang.org/x/net/html"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -10,4 +11,15 @@ import (
 func dropsau() {
 	dropsauURL := "https://dropsau.com/pages/memberships"
 	doc, err := goquery.NewDocument(dropsauURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	soldoutText := doc.Find("#sold-out")
+	
+	if strings.Contains(soldoutText.Text(), "Buy now")
+		log.Println("Buy now button is available")
+		log.Println(soldoutText.Text())
+		return
 }
